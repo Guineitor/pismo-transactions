3279890d0aeb05595db88311fa09ad72f2a51a94
Guineitor <guineitor.traps236@gmail.com>
2019-03-08 00:39:47 -0300

    bootstrap spring boot gradle groovy

---
+++ src/main/groovy/com/pismotransaction/pismotransaction/PismotransactionApplication.groovy
@@ -0,0 +1,13 @@
+package com.pismotransaction.pismotransaction
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PismotransactionApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PismotransactionApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/pismotransaction/pismotransaction/PismotransactionApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.pismotransaction.pismotransaction
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class PismotransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
